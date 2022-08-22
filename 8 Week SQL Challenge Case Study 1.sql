0b7362a7d507880c6b8eb6e392fd9be716e3c4ef
Virendra Gautam <79067837+VirendraGautam@users.noreply.github.com>
2022-08-22 13:26:37 +0530

    Update 8 Week SQL Challenge Case Study 1.sql

--- 8 Week SQL Challenge Case Study 1.sql
+++ 8 Week SQL Challenge Case Study 1.sql
@@ -163,6 +163,103 @@ GROUP BY s.customer_id
 
 
 
--- Que 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+--Que 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+WITH points AS(
+	SELECT
+		s.customer_id, 
+		m.product_name, 
+		 SUM(CASE WHEN m.product_name = 'sushi'  THEN 20*m.price
+				ELSE 10*m.price END) AS points_earned
+		 FROM menu m
+	JOIN sales s
+		ON m.product_id = s.product_id 
+	GROUP BY s.customer_id, m.product_name
+)
+SELECT 
+	customer_id,	
+	SUM(points_earned) as total_points_earned
+FROM points 
+GROUP BY customer_id;
+ 					--------------------------------------------------------------------------------------
+
+
+
+
+ /* Que 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items,
+   not just sushi - how many points do customer A and B have at the end of January?  */
+
+WITH dates AS (
+	SELECT 
+		join_date,
+		DATEADD(DAY, 6, join_date) as seven_day_offer,
+		EOMONTH('2021-01-31') AS month_last_date
+	FROM members
+)
+SELECT	
+	s.customer_id, 
+	SUM( CASE WHEN m.product_name = 'sushi' THEN 20*m.price
+			  WHEN s.order_date BETWEEN mem.join_date AND d.seven_day_offer THEN 20*m.price
+			  ELSE 10* m.price END) AS point_earned
+FROM sales s
+JOIN members mem
+	ON s.customer_id = mem.customer_id
+JOIN menu m
+	ON s.product_id = m.product_id
+JOIN dates d
+	ON mem.join_date = d.join_date
+GROUP BY s.customer_id
+				--------------------------------------------------------------------------------------
+				
+				
+				
+				
+				
+
+--								  ** Bonus Question **
+
+-- Que 11.  Recreating the following table output using the available data to quickly derive insights without needing to join the underlying tables using SQL.
+
+SELECT 
+  s.customer_id,	
+  s.order_date, 
+  m.product_name, 
+  m.price, 
+  (CASE WHEN s.order_date >= join_date THEN 'Y' ELSE 'N' END) as member
+
+FROM sales s
+LEFT JOIN members mem
+	ON s.customer_id = mem.customer_id
+LEFT JOIN menu m
+	ON s.product_id = m.product_id
+					--------------------------------------------------------------------------------------
+
+
+
+
+/* Que 12.  Danny also requires further information about the ranking of customer products, 
+but he purposely does not need the ranking for non-member purchases so he expects 
+null ranking values for the records when customers are not yet part of the loyalty program.*/
+
+WITH program_members  AS 
+		(SELECT 
+		  s.customer_id,	
+		  s.order_date, 
+		  m.product_name, 
+		  m.price, 
+		  (CASE WHEN s.order_date >= join_date THEN 'Y' ELSE 'N' END) as member
+
+		FROM sales s
+		LEFT JOIN members mem
+			ON s.customer_id = mem.customer_id
+		LEFT JOIN menu m
+			ON s.product_id = m.product_id
+)
+SELECT *,
+    CASE WHEN member= 'Y' THEN RANK() OVER( PARTITION BY customer_id,member ORDER BY order_date)
+		  ELSE NULL 
+    END AS ranking
+FROM program_members;
 
 
